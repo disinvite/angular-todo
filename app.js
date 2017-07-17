96c55c2913e1129c76972b45d85629baeb218939
disinvite <disinvite@users.noreply.github.com>
2017-07-17 11:02:56 -0400

    basic checking off

--- app.js
+++ app.js
@@ -1,25 +1,45 @@
 angular.module('todolist',[])
-.service('api',function($http) {
+.service('api',function($q,$filter) {
+    var tasks = [
+        {
+            id: 1,
+            text: 'task no. 1',
+            order: 1,
+            completed: false
+        },
+        {
+            id: 2,
+            text: 'task no. 2',
+            order: 2,
+            completed: false
+        },
+        {
+            id: 3,
+            text: 'task no. 3',
+            order: 3,
+            completed: false
+        }
+    ];
+
     return {
         getAllTasks: function() {
-            return [
-                {
-                    text: 'task no. 1',
-                    order: 1
-                },
-                {
-                    text: 'task no. 2',
-                    order: 2
-                },
-                {
-                    text: 'task no. 3',
-                    order: 3
+            return tasks;
+        },
+        finishTask: function(id) {
+            tasks.forEach(function(task) {
+                if(task.id == id) {
+                    task.completed = true;
                 }
-            ];
+            });
         }
     };
 })
 .controller('ctrl',function($scope,$filter,api) {
     $scope.tasks = api.getAllTasks();
-})
 
+    $scope.finishTask = function(id) {
+        //TODO check for existence
+        api.finishTask(id);
+        $scope.tasks = api.getAllTasks();
+    }
+})
