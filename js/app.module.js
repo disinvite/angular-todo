7dd10d7988323c3f34aa593d82c4a9ae474171f1
disinvite <disinvite@users.noreply.github.com>
2017-08-02 14:47:25 -0400

    split views into individual files

--- js/app.module.js
+++ js/app.module.js
@@ -1,1 +1,5 @@
 angular.module('todolist',['ui.router','ngAnimate'])
+.run(function($http, $templateCache) {
+    $http.get('views/edit.html', { cache: $templateCache });
+    $http.get('views/table.html', { cache: $templateCache });
+});
--- js/app.routes.js
+++ js/app.routes.js
@@ -2,15 +2,15 @@ angular.module('todolist').config(function($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/');
     $stateProvider.state('table', {
         url: '/',
-        templateUrl: 'table.html',
+        templateUrl: 'views/table.html',
         controller: 'table'
     }).state('new', {
         url: '/new',
-        templateUrl: 'edit.html',
+        templateUrl: 'views/edit.html',
         controller: 'new'
     }).state('edit', {
         url: '/edit/:id',
-        templateUrl: 'edit.html',
+        templateUrl: 'views/edit.html',
         controller: 'edit'
     });
 })
