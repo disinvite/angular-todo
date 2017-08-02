12c6c5c24f978aa707fff7eb711734c5f2d2866d
disinvite <disinvite@users.noreply.github.com>
2017-08-01 21:10:26 -0400

    oops

--- js/ctrl.new.js
+++ js/ctrl.new.js
@@ -1,4 +1,4 @@
-.controller('new', ['$scope','$state','$stateParams','mockapi', function($scope,$state,$stateParams,api) {
+angular.module('todolist').controller('new', ['$scope','$state','$stateParams','mockapi', function($scope,$state,$stateParams,api) {
     $scope.form = {};
     $scope.edittext = 'New Task';
 
