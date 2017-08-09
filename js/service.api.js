46fc4d82b35429bbea9d9e0efdd81b909f5ff27b
disinvite <disinvite@users.noreply.github.com>
2017-08-08 20:34:12 -0400

    full url

--- js/service.api.js
+++ js/service.api.js
@@ -1,19 +1,19 @@
 angular.module('todolist').service('api',function($http) {
     return {
         getAllTasks: function() {
-            return $http.get('/gs/api/tasks');
+            return $http.get('http://localhost:57772/gs/api/tasks');
         },
         getTask: function(id) {
-            return $http.get('/gs/api/task/' + id);
+            return $http.get('http://localhost:57772/gs/api/task/' + id);
         },
         updateTask: function(id,data) {
-            return $http.put('/gs/api/task/' + id,data);
+            return $http.put('http://localhost:57772/gs/api/task/' + id, data);
         },
         newTask: function(data) {
-            return $http.post('/gs/api/tasks',data);
+            return $http.post('http://localhost:57772/gs/api/tasks', data);
         },
         finishTask: function(id) {
-            return $http.put('/gs/api/task/' + id,{completed:1});
+            return $http.put('http://localhost:57772/gs/api/task/' + id, {completed: 1});
         }
     };
 })
